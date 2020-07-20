9858bb76ed224b164d4a7a7d4ddf08c93d1cf5a0
Andi Yudistira <andi.yudistira81@gmail.com>
2020-07-20 14:25:02 +0700

    test

--- Tests/AnsisCoreTests/AnsisCoreTests.swift
+++ Tests/AnsisCoreTests/AnsisCoreTests.swift
@@ -1,12 +1,12 @@
 import XCTest
-@testable import AnsisCore
+@testable import AnsisServiceCore
 
 final class AnsisCoreTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(AnsisCore().text, "Hello, World!")
+        XCTAssertEqual(AnsisServiceCore().text, "Hello, World!")
     }
 
     static var allTests = [
